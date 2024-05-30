6389f0a4fb29547aae8b7ba6c87b79a476d1bd9c
PJLD <pierrejld2005@gmail.com>
2024-05-30 10:49:04 +0200

    création test méthode equals

--- src/test/java/TestPosition.java
+++ src/test/java/TestPosition.java
@@ -14,4 +14,28 @@ import static org.junit.jupiter.api.Assertions.*;
         assertEquals(false,new Position(0,0).equals(new Position(0,1)));
         assertEquals(false,new Position(0,0).equals(new Position(1,0)));
     }
+
+    @Test @Order(2)
+    void deplacementUneCase(){
+        System.out.println("Test méthode deplacementUneCase()");
+        Position positionOrigineA=new Position(0,0);
+        Position positionDestinatieA=new Position(1,0);
+        positionOrigineA.deplacementUneCase(positionDestinatieA);
+        assertEquals(true,positionOrigineA.equals(positionDestinatieA));
+
+        Position positionOrigineB=new Position(0,0);
+        Position positionDestinatieB=new Position(1,0);
+        positionOrigineB.deplacementUneCase(positionDestinatieB);
+        assertEquals(true,positionOrigineB.equals(positionDestinatieB));
+
+        Position positionOrigineC=new Position(0,0);
+        Position positionDestinatieC=new Position(1,0);
+        positionOrigineC.deplacementUneCase(positionDestinatieC);
+        assertEquals(true,positionOrigineC.equals(positionDestinatieC));
+
+        Position positionOrigineD=new Position(0,0);
+        Position positionDestinatieD=new Position(1,0);
+        positionOrigineD.deplacementUneCase(positionDestinatieD);
+        assertEquals(true,positionOrigineD.equals(positionDestinatieD));
+    }
 }
